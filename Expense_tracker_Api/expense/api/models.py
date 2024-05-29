31b71561f28aac40c24bfdbf172b184ae67cca3c
donfortune <donfortunet.df@gmail.com>
2024-05-29 22:19:46 +0100

    added files

--- Expense_tracker_Api/expense/api/models.py
+++ Expense_tracker_Api/expense/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
--- Expense_tracker_Api/expense/api/views.py
+++ Expense_tracker_Api/expense/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.contrib.auth.forms import UserCreationForm, UserLoginForm
+
+from django.contrib.auth import login, authenticate
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('register')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+
+
+
+        
